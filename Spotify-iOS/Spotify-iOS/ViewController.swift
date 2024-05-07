92fab919e625ac3c21e3946a30eeeb4e7ec9fc97
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-05-07 14:00:45 +0900

    [Setting] #1 - 기본 프로젝트 세팅

---
+++ Spotify-iOS/Spotify-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify-iOS
+//
+//  Created by 이명진 on 5/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
