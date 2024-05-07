a206cd226f1550270d288398df7afff79d9aea01
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-05-07 19:22:20 +0900

    [Setting] #1 - 기본 폴더링 세팅

---
+++ Spotify-iOS/Spotify-iOS/TabBar/SpotifyTabBarController.swift
@@ -0,0 +1,62 @@
+//
+//  SpotifyTabBarController.swift
+//  Spotify-iOS
+//
+//  Created by 이명진 on 5/7/24.
+//
+
+import UIKit
+
+import Then
+
+final class SpotifyTabBarController: UITabBarController {
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setTabBarControllers()
+    }
+}
+
+// MARK: - Methods
+
+extension SpotifyTabBarController {
+    private func setUI() {
+        tabBar.do {
+            $0.backgroundColor = .black
+            $0.unselectedItemTintColor = .darkGray
+            $0.tintColor = .green
+            $0.layer.cornerRadius = 20
+            $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        }
+    }
+    
+    private func setTabBarControllers() {
+        let tabBarItems: [SpotifyTabBarItem] = [
+            .home,
+            .search,
+            .myLibrary,
+            .premium
+        ]
+        
+        viewControllers = tabBarItems.map { item in
+            return templateNavigationController(
+                title: item.title,
+                unselectedImage: item.unselectedImage,
+                selectedImage: item.selectedImage,
+                rootViewController: item.viewController
+            )
+        }
+    }
+    
+    private func templateNavigationController(title: String, unselectedImage: UIImage?, selectedImage: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        return UINavigationController(rootViewController: rootViewController).then {
+            $0.title = title
+            $0.tabBarItem.image = unselectedImage
+            $0.tabBarItem.selectedImage = selectedImage
+            $0.navigationBar.isHidden = true
+        }
+    }
+}
