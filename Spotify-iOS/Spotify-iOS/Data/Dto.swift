6196c555f5d62b8600795c4aa435b22a43931046
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-05-11 14:26:00 +0900

    [Setting] #main - 폴더링 2nd 세팅

---
+++ Spotify-iOS/Spotify-iOS/Data/Dto.swift
@@ -0,0 +1,8 @@
+//
+//  Dto.swift
+//  Spotify-iOS
+//
+//  Created by 이명진 on 5/11/24.
+//
+
+import Foundation
---
+++ Spotify-iOS/Spotify-iOS/Network/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  Spotify-iOS
+//
+//  Created by 이명진 on 5/11/24.
+//
+
+import Foundation
